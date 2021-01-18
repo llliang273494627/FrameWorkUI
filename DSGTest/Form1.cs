b31dae47667fe63737e95ff1e4f9a99fe3b2d40e
273494627 <273494627@qq.com>
2021-01-18 09:00:30 +0800

    添加Net项目

--- DSGTest/Form1.cs
+++ DSGTest/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FrameWorkUI
+namespace DSGTest
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,6 @@ namespace FrameWorkUI
         {
             InitializeComponent();
         }
+
     }
 }
---
+++ DSGTest/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSGTest
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+
+            
+            Application.Run(new Form1());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
